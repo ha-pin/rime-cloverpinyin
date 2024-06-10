cb63460571098c701db91619eb40008fadeda63e
HerbertHe <Herbert.He0229@gmail.com>
2024-06-11 00:33:11 +0800

    fix :bug: : fix path

--- pack.sh
+++ pack.sh
@@ -4,19 +4,19 @@ set -e
 ver="$1"
 
 cd ./rime-cloverpinyin/data/hapin_arabic
-rm -rf clover_hapin_arabic.schema-$ver.zip
+rm -rf ../clover_hapin_arabic.schema-$ver.zip
 # rm -rf clover_hapin_arabic.schema-build-$ver.zip
-zip -5 clover_hapin_arabic.schema-$ver.zip *.yaml ../opencc/*
+zip -5 ../clover_hapin_arabic.schema-$ver.zip *.yaml ../opencc/*
 # zip -5 clover_hapin_arabic.schema-build-$ver.zip *.yaml ../opencc/* ../build/*
 
 cd ../hapin_cyrillic
-rm -rf clover_hapin_cyrillic.schema-$ver.zip
+rm -rf ../clover_hapin_cyrillic.schema-$ver.zip
 # rm -rf clover_hapin_cyrillic.schema-build-$ver.zip
-zip -5 clover_hapin_cyrillic.schema-$ver.zip *.yaml ../opencc/*
+zip -5 ../clover_hapin_cyrillic.schema-$ver.zip *.yaml ../opencc/*
 # zip -5 clover_hapin_cyrillic.schema-build-$ver.zip *.yaml ../opencc/* ../build/*
 
 cd ../hapin_mixed_arabic_cyrillic
-rm -rf clover_hapin_mixed_arabic_cyrillic.schema-$ver.zip
+rm -rf ../clover_hapin_mixed_arabic_cyrillic.schema-$ver.zip
 # rm -rf clover_hapin_mixed_arabic_cyrillic.schema-build-$ver.zip
-zip -5 clover_hapin_mixed_arabic_cyrillic.schema-$ver.zip *.yaml ../opencc/*
+zip -5 ../clover_hapin_mixed_arabic_cyrillic.schema-$ver.zip *.yaml ../opencc/*
 # zip -5 clover_hapin_mixed_arabic_cyrillic.schema-build-$ver.zip *.yaml ../opencc/* ../build/*
